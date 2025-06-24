ea697d6521d32465a1eca770967415603da816c1
Yash708262 <yash708262@gmail.com>
2025-06-24 22:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	 if (!str) return []; // Handle null or undefined input
+
+    const chunks = [];
+    for (let i = 0; i < str.length; i += length) {
+        chunks.push(str.slice(i, i + length));
+    }
+    return chunks;
 }
 
 // Do not change the code below
