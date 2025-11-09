d604e9de6d27a689877a5b26ac93e50eecaf425e
Yash708262 <yash708262@gmail.com>
2025-11-09 19:17:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function stringChop(str, size) {
   // your code here
+	 if (!str) return [];
+
+  // Convert size to number (because prompt() returns string)
+  size = Number(size);
+
+  const result = [];
+
+  // Loop through the string, jumping by 'size' each time
+  for (let i = 0; i < str.length; i += size) {
+    // slice string from i to i + size
+    result.push(str.slice(i, i + size));
+  }
+
+  return result;
 }
 
 // Do not change the code below
